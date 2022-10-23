14ba5cd34d7b25d0fa98e7d8103fcd85b75f10f9
matheus-giordani <matheus.giordanioliveira@gmail.com>
2022-10-22 21:23:27 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/sign", (req, res) => {
+  const email = "mgso@ic.ufal.com.br";
+  const password = "123456";
+  console.log(req.body.email)
+  console.log(req.body.senha)
+  if (req.body.email === email && req.body.senha === password) {
+    const data = {
+      nome: "Matheus Giordani",
+      email,
+      role: ["sysAdmin"],
+    };
+     
+
+    const token = jwt.sign({ data }, "SECRET", {
+      expiresIn: 100000,
+    });
+
+    return res.json({ token: token });
+  }
+
+  res.status(500).json({ message: "Usuário ou senha incorreta" });
+});
+
+app.listen(port, () => {
+  console.log(`Link => http://localhost:${port}`);
+});
